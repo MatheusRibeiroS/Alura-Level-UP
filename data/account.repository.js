b0fc7430e2b821239a2b0838ebdaa5413dc51fc2
MatheusRibeiroS <matheus.ribeiro.mrs762@gmail.com>
2022-10-20 13:30:07 -0300

    Simulando um banco de dados

---
+++ data/account.repository.js
@@ -0,0 +1,29 @@
+import fs, { existsSync } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
+
+
+export async function save(user) {
+  if (!existsSync('./data/accounts.json') || fs.statSync('./data/accounts.json').size === 0) {
+    await writeFile('./data/accounts.json', JSON.stringify([user]));
+    return
+  }
+
+  const data = await readFile('./data/accounts.json', 'utf-8');
+
+  if (data) {
+    const accounts = JSON.parse(data);
+    accounts.push(user);
+    await writeFile('./data/accounts.json', JSON.stringify(accounts));
+  } else {
+    await writeFile('./data/accounts.json', JSON.stringify([user]));
+  }
+
+  return user;
+}
+
+export async function list() {
+  // read objects from accounts.json file, transform to json format and return
+  let accounts = [];
+  accounts = JSON.parse(await readFile('data/accounts.json', 'utf8'));
+  return accounts;
+}
