5cc5cd5bf1faabdc34d35a2b46d946af438c72d6
MatheusRibeiroS <matheus.ribeiro.mrs762@gmail.com>
2022-12-10 00:00:00 -0300

    fix: typo

--- stories/src/API/repository/story.repository.ts
+++ stories/src/API/repository/story.repository.ts
@@ -1,5 +1,5 @@
 import { CreateStoryDTO } from "../../dtos/create-story.dto.js";
-// Story entity is used when all data for a created story on the database is mandatory
+// Story entity is used when all properties for a created story on the database are mandatory
 import { StoryEntity } from "../../entities/story.entity.js";
 // same structure as the Story Entity but it has some optional database properties 
 import { StoryInterface } from "../../interfaces/interfaces.js";
